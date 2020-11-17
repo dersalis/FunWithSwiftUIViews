8416ddedded588afc6258442ae9f91c2607b2466
Damian Ruta <damian.ruta@hotmail.com>
2020-11-17 14:11:42 +0100

    Initial Commit

---
+++ FunWithSwiftUIViews/FunWithSwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunWithSwiftUIViewsApp.swift
+//  FunWithSwiftUIViews
+//
+//  Created by Damian Ruta on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FunWithSwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
